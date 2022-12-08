998949a5d888487b7409dd22d7741bd7af1ace3d
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-12-08 14:21:08 +0300

    GetPlatformById ,CreatePlatform metodları yazıldı.

--- Controllers/PlatformController.cs
+++ Controllers/PlatformController.cs
@@ -28,5 +28,29 @@ namespace PlatformService.Controllers
 
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
         }
+        
+        [HttpGet("{id}",Name="GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem= _repository.GetPlatformById(id);
+            if(platformItem!=null)
+        {
+            return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+        }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto )
+        {
+            var platformModel= _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatforms(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById),new { Id=platformReadDto.Id}, platformReadDto);
+
+        }
     }
 }
